9808fd1866ab35b2de1b57e5e36f5699a76ce49b
SunkyeongLee <sunkyong97658@gmail.com>
2024-03-26 14:31:43 +0900

    #6 code annotation added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Header, Request
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 import re
 import pymysql.cursors
@@ -22,11 +22,18 @@ API handler that functions the followings;
 2. validate the user password in accordance with regulations
 3. checks if the given credential equates with the data in DB
 4. handles the HTTP errors
+5. validate the authorization defined in request header
 """
 
 
 @app.post("/api/v1/login")
-async def usr_login(usr_login: Login, request: Request):
+async def userLogin(usr_login: Login, request: Request) -> dict:
+    """This is the main function that gives an proper response when requested
+    :param1 str user_login: instance for Login Class
+    :param2 str request: instance for Request Class,
+    pre-built function(import from fastapi) mainly handles the request header
+    :return dict: return response in json format
+    """
     headers = request.headers
 
     success_format = {
@@ -42,8 +49,12 @@ async def usr_login(usr_login: Login, request: Request):
         "statusMessage": "Invalid Format",
     }
 
-    # 1. ID : a-zA-Z, length: less than 15 characters
+    # [Feature 1] ID : a-zA-Z, length: less than 15 characters
     def userIdCheck(user_id) -> bool:
+        """This function checks the user-id in conditions
+        :param str user_id: intake user_id
+        :return bool: True if passsed, vice versa
+        """
         if user_id is None:  # null handler
             return False
 
@@ -53,8 +64,13 @@ async def usr_login(usr_login: Login, request: Request):
         else:
             return False
 
-    # 2. Password : a-zA-Z0-9 + one special character length, more than 8, less than 20 characters
+    # [Feature 2] Password : a-zA-Z0-9 + one special character length,
+    # more than 8, less than 20 characters
     def userPWCheck(user_pw) -> bool:
+        """This function checks the user-id in conditions
+        :param str user_pw: intake user_pw
+        :return bool: True if passsed, vice versa
+        """
         if user_pw is None:  # null handler
             return False
 
@@ -69,8 +85,14 @@ async def usr_login(usr_login: Login, request: Request):
         else:
             return False
 
-    # 3. checks if the given credential equates to the data in DB
-    def credentialCheck(user_id, user_pw):
+    # [Feature 3] checks if the given credential equates to the data in DB
+    def credentialCheck(user_id, user_pw) -> str:
+        """This function connects MySQL and check if the params given are corresponding
+        to the data in DB
+        :param1 str user_id: intake user_pw
+        :param2 str user_id: intake user_pw
+        :return str: return Room_code if successed, return Null if failed
+        """
         connection = pymysql.connect(
             host="localhost",
             user="user1",
@@ -86,17 +108,24 @@ async def usr_login(usr_login: Login, request: Request):
                 )
                 cursor.execute(sql)
                 result = cursor.fetchone()
-            connection.commit()  # connection is not autocommit by default. So you must commit to save your changes.
+            connection.commit()  # connection is not autocommit by default.
+            # So you must commit to save your changes.
 
         return result
 
     # final
+    """return a final result when userLogin function executes"""
     while True:
         try:
-            if headers.get("Authorization") == "test":
-                if userIdCheck(usr_login.user_id) and userPWCheck(usr_login.user_pw):
-                    query = credentialCheck(usr_login.user_id, usr_login.user_pw)
-
+            if (
+                headers.get("Authorization") == "test"
+            ):  # [Function 5] validate the authorization defined in request header
+                if userIdCheck(usr_login.user_id) and userPWCheck(
+                    usr_login.user_pw
+                ):  # [Function 1, 2] check userID, userPw credential in regulations
+                    query = credentialCheck(
+                        usr_login.user_id, usr_login.user_pw
+                    )  # [Function 3] check the given credential with DB
                     if query is None:
                         fail_format["status"] = "400"
                         fail_format["statusMessage"] = "Unauthorized"
@@ -114,7 +143,7 @@ async def usr_login(usr_login: Login, request: Request):
                 fail_format["status"] = "400"
                 fail_format["statusMessage"] = "Unauthorized"
                 return fail_format
-        except (RuntimeError, TypeError):
+        except (RuntimeError, TypeError):  # [Function 4] check runtime error
             fail_format["status"] = "700"
             fail_format["statusMessage"] = "Runtime Error"
             return fail_format
