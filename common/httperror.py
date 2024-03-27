d56dc0e877f6d114c389d75e399ed9ae1e07e213
SunkyeongLee <sunkyong97658@gmail.com>
2024-03-27 15:16:29 +0900

    #6 separate id/pw function into a different file

--- common/httperror.py
+++ common/httperror.py
@@ -1,44 +1,41 @@
-http_response_true = {
-    "ifLogin": True,
-    "roomCode": "rand",
-    "status": "200",
-    "statusMessage": "Success",
-}
-
-http_response_false = {
-    "ifLogin": False,
-    "status": "500",
-    "statusMessage": "Invalid Format",
-}
-
-error_code = {
-    300: "bad request",
-    400: "unauthorized",
-    500: "invalid format",
-    700: "internal error",
-}
-
-
 class HttpCommonError:
     """This class is comprised of the methods which are related to common error from HTTP request"""
 
     def __init__(self) -> None:
-        pass
+        self.httpResponseTrue = {
+            "ifLogin": True,
+            "roomCode": "rand",
+            "status": "200",
+            "statusMessage": "Success",
+        }
 
-    def httpSignInStatus(self, error_num, data=None):
+        self.httpResponseFalse = {
+            "ifLogin": False,
+            "status": "500",
+            "statusMessage": "Invalid Format",
+        }
+
+        self.errorCode = {
+            300: "bad request",
+            400: "unauthorized",
+            500: "invalid format",
+            700: "internal error",
+        }
+
+    def httpSignInStatus(self, errorNum: int, queryResult=None):
         """
         This is a function that checks the info in the response
         and return the corresponding results
-        :param1 int error_num: error code defined in Wiki; common error code
-        :param2 str data (optional): receive query result(room number) when succeeds,
+        :param1 int errorNum: error code defined in Wiki; common error code
+        :param2 str queryResult (optional): receive query result(room number) when succeeds,
           default as Null when fails
         :return dict: return response in json format
         """
-        if error_num == 200:
-            http_response_true["roomCode"] = data
-            return http_response_true
+        if errorNum == 200:
+            self.httpResponseTrue["roomCode"] = queryResult
+            return self.httpResponseTrue
         else:
-            http_response_false["status"] = error_num
-            http_response_false["statusMessage"] = error_code[error_num]
+            self.httpResponseFalse["status"] = errorNum
+            self.httpResponseFalse["statusMessage"] = self.errorCode[errorNum]
 
-            return http_response_false
+            return self.httpResponseFalse
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
-import re
 import common.query as sql
 import common.httperror as httperror
+import common.setting as setting
 
 app = FastAPI()
 
@@ -31,60 +31,51 @@ Login API handler that functions the followings;
 
 
 @app.post("/api/v1/login")
-async def userLogin(usr_login: Login, request: Request) -> dict:
+async def userLogin(usrLogin: Login, request: Request) -> dict:
     """This is the main function that gives an proper response when requested
-    :param1 str user_login: instance for Login Class
-    :param2 str request: instance for Request Class,
+    :param1 str usrLogin: instance for Login Class
+    :param2 str request: instance for Request Class
     pre-built function(import from fastapi) mainly handles the request header
     :return dict: return response in json format
     """
-
-    # header caller
     headers = request.headers
 
     # query caller
-    login_query = sql.MySQLConnect()
-    find_id_pw_query = """SELECT room_code FROM rushhour.users
+    loginQuery = sql.MySQLConnect()
+    findIdPwQuery = """SELECT room_code FROM rushhour.users
     where user_id = "{id}" and user_pw = "{pw}"; """.format(
-        id=usr_login.user_id, pw=usr_login.user_pw
+        id=usrLogin.user_id, pw=usrLogin.user_pw
     )
 
     # Common Error Check
-    check_status = httperror.HttpCommonError()
+    checkStatus = httperror.HttpCommonError()
 
     # id, pw check
-    id_check = re.findall("[a-zA-Z]", usr_login.user_id)
-    pw_check = re.findall("[a-zA-Z0-9_\W]", usr_login.user_pw)
+    loginCheck = setting.LoginRuleCheck()
 
     # [Function 5] handles the HTTP errors
     try:
         # [Feature 1] validate the authorization defined in request header
         if headers.get("Authorization") == "test":
-            if len(usr_login.user_id) == 0 or len(usr_login.user_pw) == 0:
-                return check_status.httpSignInStatus(500)  # no input
-            else:  # [Function 2, 3] check userID, userPw credential in regulations
-                if (
-                    len(usr_login.user_id) == len(id_check)
-                    and len(usr_login.user_id) <= 15
-                ) and (
-                    len(usr_login.user_pw) == len(pw_check)
-                    and len(re.findall("[_\W]", usr_login.user_pw)) == 1
-                    and len(usr_login.user_pw) >= 8
-                    and len(usr_login.user_pw) <= 20
+            if len(usrLogin.user_id) == 0 or len(usrLogin.user_pw) == 0:
+                return checkStatus.httpSignInStatus(500)  # no input
+            else:  # [Function 2, 3] check user_id, user_pw credential in regulations
+                if loginCheck.userIdCheck(usrLogin.user_id) and loginCheck.userPWCheck(
+                    usrLogin.user_pw
                 ):
                     # [Function 4] checks if the given credential equates with the data in DB
-                    query = login_query.queryData(find_id_pw_query)
+                    query = loginQuery.queryData(findIdPwQuery)
                     if query is None:
-                        return check_status.httpSignInStatus(400)  # unsearchable
+                        return checkStatus.httpSignInStatus(400)  # unsearchable
 
                     else:
-                        return check_status.httpSignInStatus(
+                        return checkStatus.httpSignInStatus(
                             200, query["room_code"]
                         )  # if succeed
 
                 else:
-                    return check_status.httpSignInStatus(500)  # id, pw regulation check
+                    return checkStatus.httpSignInStatus(500)  # id, pw regulation check
         else:
-            return check_status.httpSignInStatus(400)  # handler check
-    finally:
-        return check_status.httpSignInStatus(300)  # runtime error
+            return checkStatus.httpSignInStatus(400)  # handler check
+    except Exception:
+        return checkStatus.httpSignInStatus(300)  # runtime error
